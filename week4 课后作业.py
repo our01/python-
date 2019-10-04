8055269aa2a7359e8fcad79229b393719d9b9c3d
AudreyStacey <32283963+AudreyStacey@users.noreply.github.com>
2019-10-04 20:40:15 +0800

    Add files via upload

---
+++ week4 课后作业.py
@@ -0,0 +1,39 @@
+from geometry.area.circle import circle_area
+circle_area(3)
+
+from geometry.area.rectangle import rectangle_area
+rectangle_area(3,2)
+
+from geometry.area.square import square_area
+square_area(5)
+
+from geometry.area.triangles import triangles_area
+triangles_area(3,4)
+
+print("\n")
+
+from geometry.surface_area.cube import cube_sur
+cube_sur(3)
+
+from geometry.surface_area.cuboid import cuboid_sur
+cuboid_sur(3,4,5)
+
+from geometry.surface_area.cylinder import cylinder_sur
+cylinder_sur(3,5)
+
+from geometry.surface_area.sphere import sphere_sur
+sphere_sur(3)
+
+print("\n")
+
+from geometry.volume.cube import cube_vol
+cube_vol(3)
+
+from geometry.volume.cuboid import cuboid_vol
+cuboid_vol(3,6,8)
+
+from geometry.volume.cylinder import cylinder_vol
+cylinder_vol(3,10)
+
+from geometry.volume.sphere import sphere_vol
+sphere_vol(7)
